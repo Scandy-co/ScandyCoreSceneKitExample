fd629cb89e074a65c4dffbc2ea09d5c8fe44f594
H. Cole Wiley <cole@scandy.co>
2018-11-20 15:05:58 -0600

    Initial Commit

---
+++ ScandyCoreSceneKitExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScandyCoreSceneKitExample
+//
+//  Created by H. Cole Wiley on 11/20/18.
+//  Copyright © 2018 Scandy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScandyCoreSceneKitExample/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  ScandyCoreSceneKitExample
+//
+//  Created by H. Cole Wiley on 11/20/18.
+//  Copyright © 2018 Scandy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
